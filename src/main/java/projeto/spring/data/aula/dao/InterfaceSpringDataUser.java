e542137b98dd336a201437a947769a51ccb09403
Bruno Wosiak <31929337+wosiakkk@users.noreply.github.com>
2019-08-29 12:22:16 -0300

    Testes com @Query. Exemplo de testes de delete, consulta e update customizados com parâmtros

--- src/main/java/projeto/spring/data/aula/dao/InterfaceSpringDataUser.java
+++ src/main/java/projeto/spring/data/aula/dao/InterfaceSpringDataUser.java
@@ -1,12 +1,41 @@
 package projeto.spring.data.aula.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import projeto.spring.data.aula.model.UsuarioSpringData;
 
 /*Para cada classe persistente é feito um repository*/
 
 @Repository
-public interface InterfaceSpringDataUser extends CrudRepository<UsuarioSpringData, Long> {}
+public interface InterfaceSpringDataUser extends CrudRepository<UsuarioSpringData, Long> {
+	//realiando consultas customizadas com @Query, criando um método de interface e uma query com JPQL na anotação
+	//os parâmetros são passado com ? e o número referente a eles
+	@Query(value = "select p from UsuarioSpringData p where p.nome like %?1%")
+	public List<UsuarioSpringData> buscaPorNome(String nome);
+	
+	@Query(value = "select p from UsuarioSpringData p where p.nome = :paramnome")
+	public UsuarioSpringData buscaPorNomeParam(@Param("paramnome") String paramnome);
+	
+	
+	//delete condicional, alem da query customizada é necessário inicar uma transação para aoperação no bd com a anotação @Transactional
+	//e com a anotação @Modifying informa que esse método vai realizar uma alteração no banco de dados
+	@Modifying
+	@Transactional
+	@Query(value = "delete from UsuarioSpringData p where p.nome = ?1")
+	public void deletePorNome(String nome);
+	
+	
+	//update condicional
+	@Modifying
+	@Transactional
+	@Query(value = "update UsuarioSpringData p set p.email = ?1 where p.nome = ?2")
+	public void updateEmailPorNome(String email, String nome);
+}
 		
--- src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
+++ src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
@@ -1,5 +1,6 @@
 package projeto.spring.data.aula;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Test;
@@ -11,36 +12,39 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import projeto.spring.data.aula.dao.InterfaceSpringDataUser;
 import projeto.spring.data.aula.model.UsuarioSpringData;
 
-@RunWith(SpringJUnit4ClassRunner.class) //anotação para a integração do spring com junit
-@ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"}) //definindo o caminho de configuração spring
+@RunWith(SpringJUnit4ClassRunner.class) // anotação para a integração do spring com junit
+@ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" }) // definindo o caminho de configuração
+																				// spring
 public class AppSpringDataTest {
-	
-	@Autowired //anotação de injeção de dependencia, alternativa ao @inject
+
+	@Autowired // anotação de injeção de dependencia, alternativa ao @inject
 	private InterfaceSpringDataUser interfaceSpringDataUser;
-	
-	
+
 	@Test
 	public void testeInsert() {
-		
+
 		UsuarioSpringData usuario = new UsuarioSpringData();
 		usuario.setEmail("teste@teste.com");
-		usuario.setNome("Quinto Teste");
+		usuario.setNome("Eita");
 		usuario.setIdade(20);
 		usuario.setLogin("teste123");
 		usuario.setSenha("senha123");
-		
+
 		interfaceSpringDataUser.save(usuario);
-		
-		System.out.println("Total de usuário cadastrados: "+ interfaceSpringDataUser.count());
-		
+
+		System.out.println("Total de usuário cadastrados: " + interfaceSpringDataUser.count());
+
 	}
-	
+
 	@Test
 	public void testeConsulta() {
-	
-		//O find by id do spring retorna um objeto Optional do java util, por isso é necessário um obj desse tipo receber o retorno
-		Optional<UsuarioSpringData> usuario  = interfaceSpringDataUser.findById(3L);
-		//para a cessar os métodos do objeto do tipo UsuarioSpringData, usamos o método get() do optional que retorna o objeto do tipo, para dai ter acesso aos gettres do usuário
+
+		// O find by id do spring retorna um objeto Optional do java util, por isso é
+		// necessário um obj desse tipo receber o retorno
+		Optional<UsuarioSpringData> usuario = interfaceSpringDataUser.findById(3L);
+		// para a cessar os métodos do objeto do tipo UsuarioSpringData, usamos o método
+		// get() do optional que retorna o objeto do tipo, para dai ter acesso aos
+		// gettres do usuário
 		System.out.println(usuario.get().getNome());
 		System.out.println(usuario.get().getLogin());
 		System.out.println(usuario.get().getEmail());
@@ -48,10 +52,10 @@ public class AppSpringDataTest {
 		System.out.println(usuario.get().getIdade());
 		System.out.println(usuario.get().getId());
 	}
-	
+
 	@Test
 	public void testeConsultaTodos() {
-		//o find all retorna um objeto iterable, que é um tipo de lista		
+		// o find all retorna um objeto iterable, que é um tipo de lista
 		Iterable<UsuarioSpringData> lista = interfaceSpringDataUser.findAll();
 		for (UsuarioSpringData usuarioSpringData : lista) {
 			System.out.println(usuarioSpringData.getNome());
@@ -63,24 +67,68 @@ public class AppSpringDataTest {
 			System.out.println("********************************");
 		}
 	}
-	
+
 	@Test
 	public void testeUpdate() {
-		//consultando o objeto
+		// consultando o objeto
 		Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(3L);
-		//pegando0 o objeto UsuarioSpringata de dentro do objeto Optional
+		// pegando0 o objeto UsuarioSpringata de dentro do objeto Optional
 		UsuarioSpringData data = usuarioSpringData.get();
-		//atualizando o nome
+		// atualizando o nome
 		data.setNome("Terceiro UPDATE");
 		interfaceSpringDataUser.save(data);
 	}
-	
+
 	@Test
 	public void testeDelete() {
-		//primeira forma
-		//interfaceSpringDataUser.deleteById(5L);
-		//segunda
-		Optional<UsuarioSpringData> usuario  = interfaceSpringDataUser.findById(5L);
+		// primeira forma
+		// interfaceSpringDataUser.deleteById(5L);
+		// segunda
+		Optional<UsuarioSpringData> usuario = interfaceSpringDataUser.findById(5L);
 		interfaceSpringDataUser.delete(usuario.get());
 	}
+
+	// query customizada para like
+	@Test
+	public void testeConsultaNome() {
+
+		List<UsuarioSpringData> lista = interfaceSpringDataUser.buscaPorNome("Segundo");
+
+		for (UsuarioSpringData usuarioSpringData : lista) {
+			System.out.println(usuarioSpringData.getNome());
+			System.out.println(usuarioSpringData.getLogin());
+			System.out.println(usuarioSpringData.getEmail());
+			System.out.println(usuarioSpringData.getSenha());
+			System.out.println(usuarioSpringData.getIdade());
+			System.out.println(usuarioSpringData.getId());
+			System.out.println("********************************");
+		}
+
+	}
+
+	// query customizada nome específico
+	@Test
+	public void testeConsultaNomeParam() {
+
+		UsuarioSpringData usuarioSpringData = interfaceSpringDataUser.buscaPorNomeParam("Quarto Teste");
+
+		System.out.println(usuarioSpringData.getNome());
+		System.out.println(usuarioSpringData.getLogin());
+		System.out.println(usuarioSpringData.getEmail());
+		System.out.println(usuarioSpringData.getSenha());
+		System.out.println(usuarioSpringData.getIdade());
+		System.out.println(usuarioSpringData.getId());
+
+	}
+
+	@Test
+	public void testeDeletePorNome() {
+		interfaceSpringDataUser.deletePorNome("Eita");
+	}
+	
+	@Test
+	public void testeUpdateEmailPorNome() {
+		interfaceSpringDataUser.updateEmailPorNome("brunofwosiak@gmail.com", "Quarto Teste");
+	}
+	
 }
